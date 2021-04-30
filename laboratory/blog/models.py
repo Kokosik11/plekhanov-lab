7575f73cea916742d4e8168581280dbc1f4ade77
Tim <demidovbest@gmail.com>
2021-04-30 14:07:41 +0300

    blog app created, adding base app files, blog models.py starts

---
+++ laboratory/blog/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+STATUS = (
+  (0, "Draft"),
+  (1, "Publish")
+)
+
+class Post(models.Model):
+  title = models.CharField("Название", max_length=200)
+  slug = models.SlugField("Ссылка", max_length=130, unique=True, help_text="Ссылка на пост")
+  updated_on = models.DateTimeField(auto_now=True)
+  content = models.TextField() # Need to connect tinymice
+  created_on = models.DateTimeField(auto_now_add=True)
+  status = models.IntegerField(choices=STATUS, default=0)
+
+  def __str__(self):
+    return self.title
+
+  class Meta:
+    ordering = ['-created_on']
+    verbose_name = "Пост"
+    verbose_name_plural = "Посты"
