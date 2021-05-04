a73ddb5dee6996ce2c39d0e5d89d58339bbca8bd
Tim <demidovbest@gmail.com>
2021-05-04 14:12:34 +0300

    added project model + added image field on both mainpage database models

--- laboratory/mainpage/models.py
+++ laboratory/mainpage/models.py
@@ -13,6 +13,7 @@ class Post(models.Model):
   created_on = models.DateTimeField(auto_now_add=True)
   status = models.IntegerField(choices=STATUS, default=0)
   is_head = models.BooleanField(default=False)
+  image = models.ImageField("Фото", upload_to='post_images', blank=True)
 
   def __str__(self):
     return self.title
@@ -21,3 +22,22 @@ class Post(models.Model):
     ordering = ['-created_on']
     verbose_name = "Пост"
     verbose_name_plural = "Посты"
+
+
+class Project(models.Model):
+  name = models.CharField("Название проекта", max_length=200)
+  slug = models.SlugField("Ссылка", max_length=130, unique=True, help_text="Ссылка на проект")
+  updated_on = models.DateTimeField(auto_now=True)
+  content = models.TextField()
+  created_on = models.DateTimeField(auto_now_add=True)
+  status = models.IntegerField(choices=STATUS, default=0)
+  year = models.PositiveSmallIntegerField("Год", default=2021)
+  image = models.ImageField("Фото", upload_to='project_images', blank=True)
+
+  def __str__(self):
+    return self.name
+
+  class Meta:
+    ordering = ['-created_on']
+    verbose_name = "Проект"
+    verbose_name_plural = "Проекты"
