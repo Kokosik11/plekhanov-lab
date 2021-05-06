e72dabb268fa6a8a38f781da1136ff9e39228209
Tim <demidovbest@gmail.com>
2021-05-06 17:21:26 +0300

    added gmail smtp settings, need to create forms logic

--- laboratory/laboratory/settings.py
+++ laboratory/laboratory/settings.py
@@ -87,6 +87,12 @@ DATABASES = {
     }
 }
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'laboratoryrea@gmail.com'
+EMAIL_HOST_PASSWORD = 'ausqpdlliutsrmql' #past the key or password app here
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
