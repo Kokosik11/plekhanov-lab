bef865163c8e0b4e3438bb5db261320a08c05ecb
Tim <demidovbest@gmail.com>
2021-05-07 11:54:06 +0300

    deleting forms app and other connected files

--- laboratory/forms/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Feedback
-
-@admin.register(Feedback)
-class FeedbackAdmin(admin.ModelAdmin):
-  list_display = ('first_name', 'email', 'phone', 'timestamp')
-  list_display_links = ('first_name', 'email', 'phone',)
-  search_fields = ('first_name', 'email', 'phone',)
-  ordering = ['timestamp']
--- laboratory/laboratory/settings.py
+++ laboratory/laboratory/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'mainpage.apps.MainpageConfig',
-    'forms.apps.FormsConfig',
 ]
 
 MIDDLEWARE = [
